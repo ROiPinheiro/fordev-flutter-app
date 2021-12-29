1edfe7ddc35ce626511fd289a471f75601af9a45
Rafael Oliveira Pinheiro <rafael_oliveira05@hotmail.com.br>
2021-12-29 14:37:53 -0300

    ensure RemoteAuthentication throws InvalidCredentialsError on 401

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -74,4 +74,17 @@ void main() {
 
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test('Should throw InvalidCredentialError if HttpClient return 401',
+      () async {
+    when(httpClient.request(
+      url: anyNamed('url'),
+      method: anyNamed('method'),
+      body: anyNamed('body'),
+    )).thenThrow(HttpError.unauthorized);
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.invalidCredentials));
+  });
 }
