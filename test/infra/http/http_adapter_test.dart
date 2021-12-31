8d9da808037a94d7fb79f346bd37a9c948b52a55
Rafael Oliveira Pinheiro <rafael_oliveira05@hotmail.com.br>
2021-12-31 16:54:12 -0300

    ensure HttpAdapter calls post without body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -21,7 +21,7 @@ class HttpAdapter {
       'accept': 'application/json',
     };
 
-    final jsonBody = jsonEncode(body);
+    final jsonBody = body != null ? jsonEncode(body) : null;
     await client.post(url, headers: headers, body: jsonBody);
   }
 }
@@ -55,6 +55,15 @@ void main() {
           'accept': 'application/json',
         },
         body: '{"any_key":"any_value"}',
+      ));
+    });
+
+    test('Should call post without body', () async {
+      await sut.request(url: url, method: 'post');
+
+      verify(client.post(
+        any,
+        headers: anyNamed('headers'),
       ));
     });
   });
