b378b4f612877caf58d823f4ced7293039561d20
Rafael Oliveira Pinheiro <rafael_oliveira05@hotmail.com.br>
2022-02-03 10:25:23 -0300

    ensure HttpAdapter throws Server if invalid method is provided

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -18,6 +18,15 @@ void main() {
     sut = HttpAdapter(client);
     url = faker.internet.httpUrl();
   });
+
+  group('shared', () {
+    test('Should throw ServerError if invalid method is provided', () async {
+      final future = sut.request(url: url, method: 'invalid_method');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
+  });
+
   group('post', () {
     PostExpectation mockRequest() {
       return when(client.post(
